d8f964a1c1a4b4cbea4d0b1828208d695850137b
sameer-sarma <sameer.sarma@gmail.com>
2025-08-13 11:04:53 +0530

    corrected variable file for supabase prod

--- src/supabaseClient.tsx
+++ src/supabaseClient.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ifqkrxsgxunconjscebb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmcWtyeHNneHVuY29uanNjZWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0Nzk1NDgsImV4cCI6MjA1OTA1NTU0OH0.sOIRLNBR-L-zwNFY0qmaSk8Ov4CTFav53CtR7gwxAKc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
